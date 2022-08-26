db3acbddded6ebf1918c27b9f4a0d066b13039c2
ocandelc <oscarerick.candelacarbajal@nttdata.com>
2022-08-26 13:12:14 -0500

    commit

--- src/main/java/pe/com/nttdata/SpringBootEurekaClientGatewayApplication.java
+++ src/main/java/pe/com/nttdata/SpringBootEurekaClientGatewayApplication.java
@@ -1,9 +1,26 @@
+/**
+ * Resumen.
+ * Objeto                   : SpringBootEurekaClientGatewayApplication.java
+ * Descripción              : Clase para iniciar el gateway.
+ * Fecha de Creación        : 08/08/2022.
+ * Proyecto de Creación     : Bootcamp-30.
+ * Autor                    : Oscar Candela.
+ * ---------------------------------------------------------------------------------------------------------------------------
+ * Modificaciones
+ * Motivo                   Fecha             Nombre                  Descripción
+ * ---------------------------------------------------------------------------------------------------------------------------
+ * Bootcamp-30              17/08/2022        Mario Vásquez           Realizar la creación de un método nuevo.
+ */
+
 package pe.com.nttdata;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+/**
+ * Clase para iniciar el gateway.
+ */
 @EnableEurekaClient
 @SpringBootApplication
 public class SpringBootEurekaClientGatewayApplication {
--- src/test/java/pe/com/nttdata/SpringBootEurekaClientGatewayApplicationTest.java
+++ src/test/java/pe/com/nttdata/SpringBootEurekaClientGatewayApplicationTest.java
@@ -1,8 +1,25 @@
+/**
+ * Resumen.
+ * Objeto                   : SpringBootEurekaClientGatewayApplicationTest.java
+ * Descripción              : Clase para iniciar el gateway.
+ * Fecha de Creación        : 08/08/2022.
+ * Proyecto de Creación     : Bootcamp-30.
+ * Autor                    : Oscar Candela.
+ * ---------------------------------------------------------------------------------------------------------------------------
+ * Modificaciones
+ * Motivo                   Fecha             Nombre                  Descripción
+ * ---------------------------------------------------------------------------------------------------------------------------
+ * Bootcamp-30              17/08/2022        Mario Vásquez           Realizar la creación de un método nuevo.
+ */
+
 package pe.com.nttdata;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+/**
+ * Clase para iniciar el gateway.
+ */
 @SpringBootTest
 class SpringBootEurekaClientGatewayApplicationTest {
 
